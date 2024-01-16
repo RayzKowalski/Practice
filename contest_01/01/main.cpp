afa6fb47befada005c6e26da8572e23f4eabd197
KowalskiAnalysis <64913428+RayzKowalski@users.noreply.github.com>
2024-01-16 19:10:25 +0300

    Update main.cpp

--- contest_01/01/main.cpp
+++ contest_01/01/main.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main()
+{
+    double pi = sqrt(12) * (1 - 1 / 9.0 + 1 / (5 * pow(3, 2)) - 1 / (7 * pow(3, 3)) + 1 / (9 * pow(3, 4)) - 1 / (11 * pow(3, 5)));
+    cout << pi;
+    return 0;
+}
