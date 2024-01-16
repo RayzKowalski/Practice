679ba3a13f4424f35d81d193f061dfc44cc9a32e
KowalskiAnalysis <64913428+RayzKowalski@users.noreply.github.com>
2024-01-16 19:12:58 +0300

    Update main.go

--- contest_01/02/main.go
+++ contest_01/02/main.go
@@ -1,1 +1,20 @@
+package main
 
+import (
+	"fmt"
+)
+
+func main() {
+	var man float64 = 0.5
+
+	var in_year float64 = man * 365
+	var tree_1 float64 = in_year / 20
+	var tree_2 float64 = in_year / 32
+	if float64(int(tree_1)) != tree_1 {
+		tree_1++
+	}
+	if float64(int(tree_2)) != tree_2 {
+		tree_2++
+	}
+	fmt.Println(in_year, int(tree_2), int(tree_1))
+}
